dbed25c02111f0611c26a1618cb1ff971a256367
Sushreeta Shrestha <sushreetashrestha@gmail.com>
2023-03-29 22:22:10 +0545

    updates

--- books/views.py
+++ books/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from books.models import Book
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -7,20 +6,25 @@ from django.urls import reverse_lazy
 
 class BookList(ListView):
     model=Book
+    template_name='books/book_lists.html'
 
 class BookView(DetailView):
     model=Book
+    template_name='books/book_detail.html'
 
 class BookCreate(CreateView):
     model=Book
+    template_name='book_new.html'
     fields=['name','pages']
     success_url=reverse_lazy('book_list')
 
 class BookUpdate(UpdateView):
     model=Book
+    template_name='books/book_edit.html'
     fields=['name','pages']
     success_url=reverse_lazy('book_list')
 
 class BookDelete(DeleteView):
     model=Book
+    template_name='books/book_delete.html'
     success_url=reverse_lazy('book_list')
